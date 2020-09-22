1ed660e53535cfb647830a8699a5d06b09455818
Bryant Huang <b.luv.sic@gmail.com>
2020-09-21 19:10:09 -0700

    Updates

---
+++ precache-manifest.bce52095c600fb167d6b119fc55543c0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a5162d5b4596241a2332697d51f4f96",
+    "url": "/striim-cra/index.html"
+  },
+  {
+    "revision": "097c50d2f209fa2929fd",
+    "url": "/striim-cra/static/css/main.867ec186.chunk.css"
+  },
+  {
+    "revision": "4c3465bc3fc3248e978e",
+    "url": "/striim-cra/static/js/2.f462e922.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/striim-cra/static/js/2.f462e922.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "097c50d2f209fa2929fd",
+    "url": "/striim-cra/static/js/main.4bd5131d.chunk.js"
+  },
+  {
+    "revision": "402e1598d3efb22aea82",
+    "url": "/striim-cra/static/js/runtime-main.b6f0a862.js"
+  }
+]);
